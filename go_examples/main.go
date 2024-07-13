13d82c2546fa708532429facd41694b3545010f7
root <root@Lavih-B-WS.esx.mdclone>
2024-07-13 19:06:38 +0300

    Chore: update

--- go_examples/main.go
+++ go_examples/main.go
@@ -14,33 +14,18 @@ func main() {
 	var TicketRemain int = 30
 	var Bookings[]string
 	var FirstName string
+
+	// run in infinite loop the code
 	for {
 
-		fmt.Printf("Welcome to our Tickets app \nBuy your tickets here - %s.", StoreName)
-		fmt.Printf("\n%v Tickets available from total of %v\n", TicketRemain,TicketCount)
+		// greet users
+		greetUsers(StoreName,TicketRemain,TicketCount)
 
-		
-
-		// Request Name and number of tickets
-		var UserName string
-		var UserTickets int
-
-		// to allow users to enter first and last name together with space we need to use io package
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Printf("Please input your full name:\n")
-		UserName, err := reader.ReadString('\n')
-		UserName = strings.TrimSpace(UserName) // Remove the newline character
-		if err != nil {
-			fmt.Println("An error occurred when parsing user input:", err)
-		}
-		// validate user name
-		if len(UserName) < 4 {
-			fmt.Printf("Invalid full name was provided. Please Register again.\n")	
-			continue			
-		}
-		
+		// call function getUserName to get the user name
+		userFullName, _ := getUserName()
 
 		// number of tickets
+		var UserTickets int
 		fmt.Printf("Please input the number of ticket you want to purchase:\n")
 		fmt.Scan(&UserTickets)
 		// exit if number of ticket is negative
@@ -54,12 +39,12 @@ func main() {
 			continue
 		}
 
-		fmt.Printf("Order of %v Tickets under the name %v successfully processed\n", UserTickets,UserName)
+		fmt.Printf("Order of %v Tickets under the name %v %v successfully processed\n", UserTickets,userFullName["firstName"],userFullName["lastName"])
 
 		// Calculate Remain Tickets
 		TicketRemain = TicketRemain - UserTickets
 		// add new booking
-		Bookings = append(Bookings,UserName)
+		Bookings = append(Bookings,userFullName["firstName"])
 
 		fmt.Printf("Remaining Tickets Count: %v\n", TicketRemain)
 		for index,element := range Bookings {
@@ -79,4 +64,48 @@ func main() {
 			break
 		}
 	}
+}
+
+// Funcs section
+func greetUsers(StoreName string,TicketRemain int,TicketCount uint) {
+
+	fmt.Printf("Welcome to our Tickets app \nBuy your tickets here - %s.", StoreName)
+	fmt.Printf("\n%v Tickets available from total of %v\n", TicketRemain,TicketCount)
+}
+
+func getUserName() (map[string]string, error){
+		//var userName string
+		var err error
+		var userFullName = make(map[string]string)
+		// loop until the enterd username is acceptable
+		for {
+			// to allow users to enter first and last name together with space we need to use io package
+			reader := bufio.NewReader(os.Stdin)
+			//fmt.Printf("Please input your full name:\n")
+			//userName, err = reader.ReadString('\n')
+			//userName = strings.TrimSpace(userName) // Remove the newline character
+
+			fmt.Printf("Please input your full name:\n")
+			tmp, err := reader.ReadString('\n')
+			userFullName["firstName"] = strings.Fields(tmp)[0]
+			userFullName["firstName"] = strings.TrimSpace(userFullName["firstName"])
+			userFullName["lastName"] = strings.Fields(tmp)[1]
+			userFullName["lastName"] = strings.TrimSpace(userFullName["lastName"])
+
+			if err != nil {
+				fmt.Println("An error occurred when parsing user input:", err)
+				continue
+			}
+
+			// validate user name
+			if err != nil {
+				fmt.Println("An error occurred when parsing user input:", err)
+				fmt.Println("Please re-enter your input")
+			} else if len(userFullName["firstName"]+userFullName["lastName"]) < 4 {
+				fmt.Printf("Invalid full name was provided. Please Register again.\n")	
+			} else {
+				break
+			}
+		}
+		return userFullName, err
 }
